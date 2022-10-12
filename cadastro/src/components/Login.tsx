3c4d9f07e6038d4621b64aab38a200dee4412090
LaBabaquita <101640412+LaBabaquita@users.noreply.github.com>
2022-10-11 21:51:13 -0300

    Add files via upload

---
+++ cadastro/src/components/Login.tsx
@@ -0,0 +1,44 @@
+import { Dispatch, FormEventHandler, SetStateAction } from "react";
+
+export default function ({setRoute}: {setRoute: Dispatch<SetStateAction<string>>}) {
+  const enviarDados: FormEventHandler<HTMLFormElement> = async ev => {
+    ev.preventDefault()
+    const { email, password } = ev.currentTarget
+
+    const request = await fetch(`/api/login/`, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value
+      })
+    })
+
+    const responseData = await request.json()
+
+    if (request.status >= 200 && request.status <= 299) {
+      localStorage.setItem("token", responseData.token)
+      alert("PARABÉNS YEY!")
+      setRoute("teste")
+      return
+    }
+
+    if (responseData.error) {
+      alert(responseData.error)
+      return
+    }
+
+    alert("Cara! deu erro aqui e eu nem sei o que foi!")
+  }
+  
+  
+  return <>
+    <form onSubmit={enviarDados}>
+      <h1>Login</h1>
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <button onClick={() => {}}>entrar</button>
+      <button onClick={() => setRoute("cadastro")}>cadastrar-se</button>
+    </form>
+  </>
+}
